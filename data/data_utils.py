7255887ee85808c4c65bcebc3a0cec96b4f1abca
y-aoub <ayoubelhoudri@gmail.com>
2024-02-21 00:26:23 +0100

    update data_utils

--- data/data_utils.py
+++ data/data_utils.py
@@ -3,11 +3,10 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import cv2
 import os
+import torch
 import numpy as np
-from tqdm import tqdm
-from PIL import Image
-
 import logging
+from torch.utils.data import Dataset, DataLoader, random_split
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
@@ -19,44 +18,99 @@ class DataPaths:
         self.PROCESSED_IMAGES_ABS_PATH = os.path.join(self.DATA_ABS_PATH, "processed_images")
         self.PROCESSED_MASKS_ABS_PATH = os.path.join(self.DATA_ABS_PATH, "processed_masks")
         
-class DataProcessing:
-    def __init__(self):
-        
+class DataUtils:
+    def __init__(self): 
         self.data_paths = DataPaths()
-    
+        
     def list_data_files_abs_path(self, abs_path: str) -> list:
         data_files_abs_path = list(map(lambda x: os.path.join(abs_path, x), os.listdir(abs_path)))
         return sorted(data_files_abs_path)
     
-    def read_raw_mask(self, file_abs_path: str):
+    def read_mat_file(self, file_abs_path: str):
         mat_data = scipy.io.loadmat(file_abs_path)
         mask = mat_data['mask']
         return mask
     
-    def read_raw_image(self, file_abs_path):
+    def read_npy_file(self, file_abs_path: str):
+        npy_data = np.load(file_abs_path)
+        return npy_data
+            
+    def read_jpg_file(self, file_abs_path):
         image = mpimg.imread(file_abs_path)
         return image
+    
+    def get_file_basename(self, file_abs_path):
+        basename = os.path.splitext(os.path.basename(file_abs_path))[0]
+        return basename
             
     def create_processed_data_dir(self):
         if not os.path.exists(self.data_paths.PROCESSED_IMAGES_ABS_PATH):
             os.makedirs(self.data_paths.PROCESSED_IMAGES_ABS_PATH)
         if not os.path.exists(self.data_paths.PROCESSED_MASKS_ABS_PATH):
-            os.makedirs(self.data_paths.PROCESSED_MASKS_ABS_PATH)
+            os.makedirs(self.data_paths.PROCESSED_MASKS_ABS_PATH)   
             
-    def read_all_raw_images(self, data_files_abs_path):
-        logging.info("Reading Images ...")
-        raw_images = list(map(lambda x: self.read_raw_image(x), tqdm(data_files_abs_path)))
-        return raw_images
-    
-    def read_all_raw_masks(self, data_files_abs_path: list):
-        logging.info("\nReading Masks ...")
-        raw_masks = list(map(lambda x: self.read_raw_mask(x), tqdm(data_files_abs_path)))
-        return raw_masks
-    
-    def merge_raw_data(self, raw_images, raw_masks):
-        merged_raw_data = list(zip(raw_images, raw_masks))
-        return merged_raw_data
-    
+    def save_data_tuple(self, image, mask, basename):
+        saved_image_abs_path = f'{self.data_paths.PROCESSED_IMAGES_ABS_PATH}/{basename}.npy' 
+        np.save(saved_image_abs_path, image)
+        saved_mask_abs_path = f'{self.data_paths.PROCESSED_MASKS_ABS_PATH}/{basename}.npy'
+        np.save(saved_mask_abs_path, mask)
+        
+    def save_data_tuple(self, image, mask, basename):
+        saved_image_abs_path = f'{self.data_paths.PROCESSED_IMAGES_ABS_PATH}/{basename}.npy' 
+        np.save(saved_image_abs_path, image)
+        saved_mask_abs_path = f'{self.data_paths.PROCESSED_MASKS_ABS_PATH}/{basename}.npy'
+        np.save(saved_mask_abs_path, mask)
+        
+    def plot_data_element(self, image, mask) -> None:
+        plt.figure(figsize=(15, 15))
+        plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Image')
+        plt.subplot(1, 2, 2), plt.imshow(mask, cmap='gray'), plt.title('Mask')
+        plt.show() 
+        
+class FormatDataset(Dataset):
+    def __init__(self, image_dir, mask_dir):
+        
+        data_utils = DataUtils()
+        
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        
+        self.image_files = data_utils.list_data_files_abs_path(image_dir)
+        self.mask_files = data_utils.list_data_files_abs_path(mask_dir)
+        
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        image_path = self.image_files[index]
+        mask_path = self.mask_files[index]
+
+        image = np.load(image_path)
+        mask = np.load(mask_path)
+
+        image = torch.FloatTensor(image).permute(2, 0, 1)  
+        mask = torch.LongTensor(mask).unsqueeze(0)
+
+        return image, mask
+
+class DataLoadersManager:
+    def __init__(self, image_dir, mask_dir, train_batch_size, val_batch_size, test_batch_size, train_size, val_size, test_size):
+        dataset = FormatDataset(image_dir, mask_dir)
+        
+        train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+        
+        self.train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+        self.val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=True)
+        self.test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
+        
+    def get_dataloaders(self):
+        return {'train': self.train_dataloader, 'val': self.val_dataloader, 'test': self.test_dataloader}
+
+class DataProcessing:
+    def __init__(self, R=400, size=400):
+        self.R = R
+        self.size = size        
+        
     def resize_mask(self, image, mask):
         _, mask_width = mask.shape[:2]
         _, image_width = image.shape[:2]
@@ -64,12 +118,6 @@ class DataProcessing:
         end_col = start_col + image_width
         cropped_mask = mask[:, start_col:end_col]
         return image, cropped_mask
-
-    
-    def resize_all_masks(self, merged_raw_data):
-        logging.info("\nResizing Masks ...")
-        merged_data = list(map(lambda x: self.resize_mask(*x), tqdm(merged_raw_data)))
-        return merged_data
     
     def detect_optic_disc_center(self, mask):
         mask_copy = np.copy(mask)
@@ -94,40 +142,15 @@ class DataProcessing:
         polar_mask = cv2.linearPolar(mask, center, R, cv2.WARP_FILL_OUTLIERS)
         return polar_mask
     
-    def polar_transformation(self, image, mask, R=400) -> None:
+    def polar_transform_data_tuple(self, image, mask) -> None:
         center = self.detect_optic_disc_center(mask)
-        polar_image = self.polar_transform_image(image, center=center, R=R)
-        polar_mask = self.polar_transform_mask(mask, center=center, R=R)
+        polar_image = self.polar_transform_image(image, center=center, R=self.R)
+        polar_mask = self.polar_transform_mask(mask, center=center, R=self.R)
         polar_image = cv2.rotate(polar_image, - cv2.ROTATE_90_CLOCKWISE)
         polar_mask = cv2.rotate(polar_mask, - cv2.ROTATE_90_CLOCKWISE)
         return polar_image, polar_mask
     
-    def polar_transformation_all_data(self, merged_data):
-        logging.info("\nPolar Transformation of Data ...")
-        polar_data = list(map(lambda x: self.polar_transformation(*x), tqdm(merged_data)))
-        return polar_data
-    
-    def resize_data_element(self, image, mask, size=400):
-        new_size = (size, size)
+    def resize_data_tuple(self, image, mask):
+        new_size = (self.size, self.size)
         resized_image, resized_mask = cv2.resize(image, new_size), cv2.resize(mask, new_size)
         return resized_image, resized_mask
-    
-    def resize_all_data(self, merged_data):
-        resized_merged_data = list(map(lambda x: self.resize_data_element(*x), merged_data))
-        return resized_merged_data
-    
-    def plot_data_element(self, image, mask) -> None:
-        plt.figure(figsize=(15, 15))
-        plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Image')
-        plt.subplot(1, 2, 2), plt.imshow(mask, cmap='gray'), plt.title('Mask')
-        plt.show()
-        
-    def save_data(self, data):
-        logging.info("\nSaving Processed Data ...")
-        for index in tqdm(range(len(data))):
-            image, mask = data[index][0], data[index][1]
-            saved_image_abs_path = f'{self.data_paths.PROCESSED_IMAGES_ABS_PATH}/{index+1}.npy' 
-            np.save(saved_image_abs_path, image)
-            saved_mask_abs_path = f'{self.data_paths.PROCESSED_MASKS_ABS_PATH}/{index+1}.npy'
-            np.save(saved_mask_abs_path, mask)
-            
